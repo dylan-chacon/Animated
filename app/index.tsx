419c1a68d5ad048f32d772a5a4a4f7f14be31b15
Dylan Chacon <dachaconp@unal.edu.co>
2025-06-06 15:43:05 -0500

    fix: root

--- app/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AppNavigator from '@/navigation/AppNavigator';
-
-export default function HomeScreen() {
-  return <AppNavigator />;
-}
-
-
--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { RootStackParamList } from './types';
 import AnimatedListScreen from '@/app/modules/animated/screens/Home/HomeScreen';
@@ -9,30 +8,28 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 const AppNavigator: React.FC = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#007BFF',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-      >
-        <Stack.Screen 
-          name="AnimatedList" 
-          component={AnimatedListScreen} 
-          options={{ title: 'Lista Animada' }} 
-        />
-        <Stack.Screen
-          name="Details"
-          component={DetailScreen}
-          options={({ route }) => ({ title: route.params.item.title })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#007BFF',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}
+    >
+      <Stack.Screen 
+        name="AnimatedList" 
+        component={AnimatedListScreen} 
+        options={{ title: 'Lista Animada' }} 
+      />
+      <Stack.Screen
+        name="Details"
+        component={DetailScreen}
+        options={({ route }) => ({ title: route.params.item.title })}
+      />
+    </Stack.Navigator>
   );
 };
 
